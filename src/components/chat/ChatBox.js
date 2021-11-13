70f924a9b4f9f6b091950819ed78ab3f112d06a9
nguyenphuctinh <phuctinh2001@gmail.com>
2021-11-13 13:22:07 +0700

    chat

--- src/components/chat/ChatBox.js
+++ src/components/chat/ChatBox.js
@@ -1,5 +1,28 @@
 import React from "react";
 
 export default function ChatBox() {
-  return <div className="chatBox">hello chat</div>;
+  return (
+    <div className="chatBox">
+      <div className="content">
+        hello chat
+        <p>helo</p>
+        <p>helo</p>
+        <p>helo</p>
+        <p>helo</p>
+        <p>helo</p>
+        <p>helo</p>
+        <p>helo</p>
+        <p>helo</p>
+        <p>helo</p>
+        <p>helo</p>
+        <p>helo</p>
+        <p>helo</p>
+        <p>helo</p>
+      </div>
+      <div className="d-flex">
+        <input type="text" />
+        <i className="far fa-arrow-circle-right "></i>
+      </div>
+    </div>
+  );
 }
