5a6d41a51838b3ea6885518b5969d3b2ffc39c5c
nguyenphuctinh <phuctinh2001@gmail.com>
2021-11-17 12:56:49 +0700

    chat

--- src/components/chat/ChatBox.js
+++ src/components/chat/ChatBox.js
@@ -1,11 +1,10 @@
-import React, { memo, useState } from "react";
+import React, { memo, useState, useEffect, useRef } from "react";
 import Message from "./Message";
 
 const id_game = 1;
 function ChatBox({ data }) {
   const [mes, setMes] = useState("");
   const handleMessage = () => {
-    console.log(mes);
     data.stompClient.send(
       `/app/xo/${id_game}/${data.id_match}`,
       {},
@@ -13,20 +12,29 @@ function ChatBox({ data }) {
     );
     setMes("");
   };
+  const messagesEndRef = useRef(null);
+  const scrollToBottom = () => {
+    messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+  useEffect(() => {
+    scrollToBottom();
+  }, [data]);
 
   return (
     <div className="chatBox mt-lg-4 ">
       <h3>Trò chuyện</h3>
       <div className="content mt-1 mb-2 ">
         {data.messages.map((e, index) => {
-          {
-            console.log(e);
-          }
           return (
-            <Message key={index} message={e} is_chat={data.type == e.type} />
+            <>
+              {" "}
+              <Message key={index} message={e} is_chat={data.type == e.type} />
+            </>
           );
         })}
+        <div ref={messagesEndRef} />
       </div>
+
       <div className="d-flex">
         <input
           placeholder="Aa"
