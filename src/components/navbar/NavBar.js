3f5adaca21e947509d597b984f5eec19620e03fe
nguyenphuctinh <phuctinh2001@gmail.com>
2021-11-17 13:26:52 +0700

    nav

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -5,7 +5,7 @@ import { UserContext } from "../../context/UserProvider";
 import logo from "../../assets/img/logo.png";
 export default function NavBar() {
   const { user } = useContext(UserContext);
- 
+  console.log(user);
   return (
     <div className="container-fluid padding-0">
       <div className="row myNavbar padding-lg">
@@ -17,21 +17,6 @@ export default function NavBar() {
           </div>
         </div>
         <div className="col-6 padding-0" style={{ textAlign: "center" }}>
-          {/* <div className="searchForm">
-            <form className="" action="/search/" method="get">
-              <input
-                id="search"
-                autocomplete="off"
-                name="q"
-                className="searchInput"
-                type="search"
-                placeholder="Bạn tìm gì..."
-                aria-label="Search"
-                onblur="document.getElementById('closeSearch').click();"
-              />
-            </form>
-            <i id="closeSearch" className="fal fa-times"></i>
-          </div> */}
           <div className="main">
             <ul className="d-flex">
               <li className="d-flex align-items-center">
@@ -52,8 +37,25 @@ export default function NavBar() {
                 ) : (
                   <>
                     <Link to="/account">
-                      <div style={{ marginRight: "2rem" }} className="bg">
-                        <i className="far fa-user"></i>
+                      <div
+                        style={{ marginRight: "2rem" }}
+                        className="bgAccount"
+                      >
+                        <div className="d-flex  justify-content-center align-items-center">
+                          <img
+                            style={{
+                              width: "1.5rem",
+                              borderRadius: "50%",
+                              backgroundColor: "white",
+                              marginRight: "3px",
+                            }}
+                            src={`https://avatars.dicebear.com/api/micah/${
+                              user.first_name + " " + user.last_name
+                            }.svg`}
+                            alt=""
+                          />
+                          <p className="m-0">{user.first_name}</p>
+                        </div>
                       </div>
                     </Link>
                     <Link to="/logout">
