6938492743ce8d05f014610569ea8bbf6f73988e
nguyenphuctinh <phuctinh2001@gmail.com>
2021-11-24 07:36:01 +0700

    f

--- src/components/pages/Nav.js
+++ src/components/pages/Nav.js
@@ -24,16 +24,16 @@ export default function Nav() {
 
   return (
     <div>
-      <Route exact path="/" component={Home}></Route>
-      <Route exact path="/news" component={NewsList}></Route>
-      <Route path="/login">
-               <Login />
+      <Switch>
+        <Route exact path="/" component={Home}></Route>
+        <Route exact path="/news" component={NewsList}></Route>
+        <Route path="/login">
+          <Login />
         </Route>
-      {user === "unload" ? (
-        <Loading />
-      ) : (
-        <>
-          <Switch>
+        {user === "unload" ? (
+          <Loading />
+        ) : (
+          <>
             <Route path="/register">
               {user ? <Redirect to="/" /> : <Register />}
             </Route>
@@ -63,11 +63,11 @@ export default function Nav() {
             <Route exact path="/admin/user">
               {!user ? <Redirect to="/login" /> : <User />}
             </Route>
-            <Route component={NotFound} />
-          </Switch>
-          <GoToTop />
-        </>
-      )}
+            <GoToTop />
+          </>
+        )}
+        <Route component={NotFound} />
+      </Switch>
     </div>
   );
 }
