d5068309ee67ec182bb62dc0d5d5b9fb2127919a
nguyenphuctinh <phuctinh2001@gmail.com>
2021-12-04 19:05:52 +0700

    web

--- src/components/home/Header.js
+++ src/components/home/Header.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { UserContext } from "../../context/UserProvider";
-
+import vid from "../../assets/img/animated-zed-odyssey-intro.webm";
 import { Link } from "react-router-dom";
 import headerImg from "../../assets/img/game.png";
 export default function Header() {
@@ -9,10 +9,28 @@ export default function Header() {
   return (
     <div className="container-fluid">
       <div className="row">
-        <div className="header">
-          <div className="container">
-            <div className="row">
-              <img className="header__img" src={headerImg} alt="" />
+        <div className="header p-0">
+          <div
+            style={{
+              position: "relative",
+            }}
+            className="zed"
+          >
+            <div className="zed__backGround"></div>
+            <video className="zed__video" playsInline loop muted autoPlay>
+              <source src={vid} type="video/mp4" />
+            </video>
+            <div className="zed__titles">
+              <p className="zed__title1">NEW GAMES</p>
+              <p className="zed__title2">League Of Legends</p>
+              <p className="zed__title3">
+                The 11 Biggest Autumn/Winter 2021 Trends
+              </p>
+
+              <div style={{ position: "relative", padding: "3rem 0" }}>
+                <p className="zed__title1">BY RIOT GAMES</p>
+                <p className="zed__title4">11 NOVEMBER 2021</p>
+              </div>
             </div>
           </div>
 
